49e76e518a888b8ec7c650f6f3e59acea9df10f0
Чайка <mellanie335@gmail.com>
2017-04-07 06:04:40 +0900

    Initial Commit

---
+++ TCPSession/TCPSession.h
@@ -0,0 +1,13 @@
+//
+//  TCPSession.h
+//  TCPSession
+//
+//  Created by Чайка on 4/7/17.
+//  Copyright © 2017 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCPSession : NSObject
+
+@end
---
+++ TCPSession/TCPSession.m
@@ -0,0 +1,13 @@
+//
+//  TCPSession.m
+//  TCPSession
+//
+//  Created by Чайка on 4/7/17.
+//  Copyright © 2017 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "TCPSession.h"
+
+@implementation TCPSession
+
+@end
