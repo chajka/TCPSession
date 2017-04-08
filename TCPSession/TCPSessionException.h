3e2ff0593cba356e705f08b15f96895a6a864a69
Чайка <mellanie335@gmail.com>
2017-04-08 12:17:58 +0900

    Add class TCPSessionException into project.

---
+++ TCPSession/TCPSessionException.h
@@ -0,0 +1,13 @@
+//
+//  TCPSessionException.h
+//  TCPSession
+//
+//  Created by Чайка on 4/8/17.
+//  Copyright © 2017 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCPSessionException : NSException
+
+@end
---
+++ TCPSession/TCPSessionException.m
@@ -0,0 +1,13 @@
+//
+//  TCPSessionException.m
+//  TCPSession
+//
+//  Created by Чайка on 4/8/17.
+//  Copyright © 2017 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "TCPSessionException.h"
+
+@implementation TCPSessionException
+
+@end
